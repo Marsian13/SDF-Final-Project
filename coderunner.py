292599e4c3325d6aeac3f1d48c3246dc6617da9d
Marsian13 <vanshg132005@gmail.com>
2025-05-01 21:34:21 +0530

    added uml and stuff

--- coderunner.py
+++ coderunner.py
@@ -5,8 +5,8 @@ def main():
         print("Please enter input in format of <int/float> <add/sub/mul/div> <First operand> <Second operand>")
         return
     
-    os.system("ant compile")
-    command = ["java" , "-cp" , "build", "MyInfArith"] + sys.argv[1:]
+    # os.system("ant jar")
+    command = ["java", "-jar", "arbitraryarithmetic/aarithmetic.jar"] + sys.argv[1:]
     subprocess.run(command)
 if __name__ == "__main__":
     main()
